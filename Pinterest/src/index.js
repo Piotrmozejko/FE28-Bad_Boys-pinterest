80d58cda928a59c015f3e7c3e9e68feef9998bcf
Piotrmozejko <mozejko89@gmail.com>
2022-07-16 19:44:48 +0300

    last

--- Pinterest/src/index.js
+++ Pinterest/src/index.js
@@ -1,28 +1,34 @@
 import { createAllLi } from "./users.js";
 import { reset_board } from "./func.js";
+import dialogWindow from "./windows.js";
 
 // import users_base from "./users.js";
-let resp = await fetch("./usersdata.json");
-const users_base = await resp.json();
-reset_board(users_base);
+let users_base = [];
+async function getUser() {
+  let resp = await fetch("./usersdata.json");
+  users_base = await resp.json();
+  reset_board(users_base);
 
-createAllLi(users_base, "Выбрать доску");
+  createAllLi(users_base, "Выбрать доску");
+  dialogWindow();
+}
+getUser();
 let ulConteiner = document.querySelector(".picture-list");
 let deskElement = document.querySelector(".drop-list");
 let searchElement = document.querySelector(".search-header");
 
 deskElement.addEventListener("input", () => {
-    ulConteiner.innerHTML = "";
-    alert(deskElement.value);
-    createAllLi(users_base, deskElement.value);
+  ulConteiner.innerHTML = "";
+  alert(deskElement.value);
+  createAllLi(users_base, deskElement.value);
 });
 
 searchElement.addEventListener("input", () => {
-    ulConteiner.innerHTML = "";
+  ulConteiner.innerHTML = "";
 
-    if (searchElement.value) {
-        createAllLi(users_base, searchElement.value);
-    } else {
-        createAllLi(users_base, deskElement.value);
-    }
+  if (searchElement.value) {
+    createAllLi(users_base, searchElement.value);
+  } else {
+    createAllLi(users_base, deskElement.value);
+  }
 });
